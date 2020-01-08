4eaf09a42c51c1c5acda1056ecf5f8b79182e322
linoskoczek <linoskoczek@protonmail.com>
2020-01-08 14:08:25 +0100

    Updated: installation in readme

--- Requester.py
+++ Requester.py
@@ -30,7 +30,7 @@ class Requester (threading.Thread):
                 self.handle_result(requests.get(url), plugin)
 
     def handle_result(self, request, plugin_name):
-        if request.status_code != Config.STATUS_CODES_NOT_FOUND:
+        if request.status_code not in Config.STATUS_CODES_NOT_FOUND:
             Storage.found_plugins.append(plugin_name)
             Printer.p(self.NAME, str(request.status_code) + '\t' + plugin_name, 1)
             Printer.f_single_append_synchronized(
